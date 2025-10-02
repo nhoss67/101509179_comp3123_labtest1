b97d1f4537f97059caaffab220e11b7bf6c3dec4
nhoss67 <126890699+nhoss67@users.noreply.github.com>
2025-10-02 16:29:09 -0400

    Create question-3-remove.js

---
+++ question-3/question-3-remove.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+const logsDir = path.join(__dirname, 'Logs');
+
+if (fs.existsSync(logsDir)) {
+    fs.readdirSync(logsDir).forEach(file => {
+        console.log(`delete files...${file}`);
+        fs.unlinkSync(path.join(logsDir, file));
+    });
+
+    fs.rmdirSync(logsDir);
+    console.log("Logs directory removed.");
+} else {
+    console.log("No Logs directory found.");
+}
