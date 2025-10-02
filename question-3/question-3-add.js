d67e5349e78187a4b050fa903c355727dc3d13d9
nhoss67 <126890699+nhoss67@users.noreply.github.com>
2025-10-02 16:28:54 -0400

    Create question-3-add.js

---
+++ question-3/question-3-add.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+const logsDir = path.join(__dirname, 'Logs');
+
+if (!fs.existsSync(logsDir)) {
+    fs.mkdirSync(logsDir);
+}
+
+process.chdir(logsDir);
+
+for (let i = 0; i < 10; i++) {
+    const fileName = `log${i}.txt`;
+    fs.writeFileSync(fileName, `Log file # ${i}`);
+    console.log(fileName);
+}
